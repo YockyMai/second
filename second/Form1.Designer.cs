a95ab0106f51d612e41ed1d043076ad6ec217a30
Grigoriev Valerii <g.valera15102003@gmail.com>
2021-10-29 04:18:27 +0500

    Revert "Нет кнопки закрыть, зеленая форма"
    This reverts commit cd3a1208c27173f02ad45fbe00e8633b282072bc.

--- second/Form1.Designer.cs
+++ second/Form1.Designer.cs
@@ -29,14 +29,26 @@ namespace second
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(845, 283);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
             this.ResumeLayout(false);
@@ -44,6 +56,8 @@ namespace second
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
