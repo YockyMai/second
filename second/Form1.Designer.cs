b9ec71eb738ce0d5a13084d46b78ec23bd4e7d68
Grigoriev Valerii <g.valera15102003@gmail.com>
2021-10-29 04:21:06 +0500

    Удлинил форму и перекрасил в голубой

--- second/Form1.Designer.cs
+++ second/Form1.Designer.cs
@@ -67,8 +67,8 @@ namespace second
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(396, 326);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(866, 311);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
